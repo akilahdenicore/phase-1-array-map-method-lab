6a64ac95435fd9d528d7eec17b38a105d6e824b2
Akilah Snowden <akilahdsnowden@gmail.com>
2022-12-06 15:30:52 -0800

    commit tree f13c8533b6f289fdbd08840344711a1cd99264ad

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split("");
+//     const capitalizedTokens = tokens.map(
+//       (token) => token.charAt(0).toUppercase() + token.slice(1)
+//     );
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   });
+// };
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
+
+
+// consider:
+
+// How can we "iterate" through individual words in a string?
+// Can we execute an iteration inside an iteration? How?
+// How can we capitalize just the first letter in a word?
